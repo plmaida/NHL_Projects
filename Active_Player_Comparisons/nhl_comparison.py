255b9d71f42d20ebb61175baf211d4b6623672d2
Paul <38801847+plmaida@users.noreply.github.com>
2019-02-12 16:49:08 -0500

    update

--- Active_Player_Comparisons/nhl_comparison.py
+++ Active_Player_Comparisons/nhl_comparison.py
@@ -20,20 +20,28 @@ parser.add_argument('--remove', type=int, default=None)
 parser.add_argument('--save', type=eval, default=False)
 parser.add_argument('--saveas', type=str, default=None)
 parser.add_argument('--load', type=str, default=None)
+parser.add_argument('--players', type=eval, default=False)
 args = parser.parse_args()
 
-
+#-----------------------------------------------------------Create functions--------------------------------------------------------
 def player_stats(p):
+    #search for the players player code
     player = get('https://suggest.svc.nhl.com/svc/suggest/v1/minactiveplayers/'+p+'/99999')
     player_data = json.loads(player.text)
     print(player_data)
-    x = input("Player ID: ")
-    nhl = get('https://statsapi.web.nhl.com/api/v1/people/'+x+'/stats/?expand=person.stats&stats=yearByYear')
+
+    #use the player id to pull information from the NHL
+    p_id = input("Player ID: ")
+    nhl = get('https://statsapi.web.nhl.com/api/v1/people/'+p_id+'/stats/?expand=person.stats&stats=yearByYear')
     nhl_data = json.loads(nhl.text)
-    #print(nhl_data)
+
+    #fix up and combine dataset into a final dataframe (result)
     df = pd.io.json.json_normalize(nhl_data['stats'], record_prefix='_',
                               record_path='splits', errors='ignore',
-                              meta=['season', ['_league', 'name'], ['_stat', 'assists']])
+                              meta=['season', ['_league', 'name'],
+                              ['_stat', 'assists']
+                              ])
+
     df_league = pd.DataFrame(df["_league"].values.tolist())
     df_league = df_league.drop(['id', 'link'], axis=1)
     df_league = df_league.rename({'name':'league'}, axis='columns')
@@ -55,28 +63,39 @@ def player_stats(p):
 
     result['Points/Game'] = result['points']/result['games']
     result['Player'] = p
+
     return(result)
 
+
 def visualize_comp(df):
+    #choose the y axis
     y = input("how do you want to compare these players (goals, assists, points, OR Points/Game): ")
-    #df_comp.groupby('Player')['season', y].plot(legend=True, title=y)
+
+    #create the plot
     fig, ax = plt.subplots(figsize=(20,10))
     cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
     i = 0
     for label, group in df.groupby('Player'):
-
         group.plot(x='season', y=y, title="Comparing "+y, ax=ax, label=label, color=cycle[i])
+
         mean_val = round(group[y].mean(), 2)
-        print(mean_val)
-        ax.annotate(mean_val, xy=(group['season'].iloc[1], mean_val), xytext=(group['season'].iloc[2], mean_val*1.1),
-                    arrowprops=dict(facecolor=cycle[i], shrink=0.05))
+        ax.annotate(mean_val, xy=(group['season'].iloc[1], mean_val),
+                    xytext=(group['season'].iloc[2], mean_val*1.1),
+                    arrowprops=dict(facecolor=cycle[i], shrink=0.05)
+                    )
+
         # vertical dotted line originating at mean value
         plt.axhline(mean_val, linestyle='dashed', linewidth=2, color=cycle[i])
         i+=1
+
+    #save the plot
     plt.savefig('./comparison.png')
+    print("Comparison Chart Saved")
+
 
 def compare_players():
     num_players = input('How many players do you want to look at: ')
+
     appended_data = []
     for i in range (int(num_players)):
         p = input("which player do you want to look at: ")
@@ -84,34 +103,47 @@ def compare_players():
         appended_data.append(pp)
     df_comp = pd.concat(appended_data, axis=0)
     visualize_comp(df_comp)
+
     if args.save or args.saveas is not None:
         save_df(df_comp)
+
     return df_comp
+
 
 def add_player(df):
     p = input("which player do you want to look at: ")
     pp = player_stats(p)
     df = pd.concat([df, pp], axis=0)
+
     if args.save or args.saveas is not None:
         save_df(df)
+
     return df
+
 
 def remove_player(df):
     p = input("which player do you want to remove: ")
     df = df[df.Player != p]
+
     if args.save or args.saveas is not None:
         save_df(df)
+
     return df
+
 
 def save_df(df, filename="player_comparison.pkl"):
     if args.saveas is not None:
         filename = args.saveas
-    elif args.load is not None:
+    elif args.load is not None: #allows for the user not to have to put a new name in
         filename = args.load
     df.to_pickle(filename)
 
 
+def list_names(df):
+    print(df['Player'].unique())
 
+
+#-----------------------------------------------------Use the parser arguments-----------------------------------------------------------
 if args.load is not None:
     df = pd.read_pickle(args.load)
     if args.save:
@@ -120,8 +152,8 @@ if args.load is not None:
 if args.compare:
     df = compare_players()
 
-if args.visualize:
-    visualize_comp(df)
+if args.players:
+    list_names(df)
 
 if args.add:
     for i in range(args.add):
@@ -130,3 +162,7 @@ if args.add:
 if args.remove:
     for i in range(args.remove):
         df = remove_player(df)
+
+#visualize should always go last
+if args.visualize:
+    visualize_comp(df)
